1712725645942cd56f4ae0a48452fb45896f05fd
DanielZornek <daniel_zornek91@outlook.com>
2024-09-26 22:08:21 -0300

    autenticavel feito | correcões  também realizadas

--- src/br/edu/fatecpg/tecprog/heranca/autenticavel/model/SistemaDeSeguranca.java
+++ src/br/edu/fatecpg/tecprog/heranca/autenticavel/model/SistemaDeSeguranca.java
@@ -7,29 +7,65 @@ public class SistemaDeSeguranca implements Autenticavel{
 	
 	private String usuario = "admin";
 	private String senha = "1234";
+	private boolean logado = false;
 	
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public void setLogado(boolean logado) {
+		this.logado = logado;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public boolean isLogado() {
+		return logado;
+	}
+
 	@Override
 	public void login(String usuario, String senha) {
-		
-		usuario = entrada.nextLine();
-		senha= entrada.nextLine();
-		
+			
 		while(true) {
-			if(usuario != this.usuario) {
-				System.out.print("Usuário incorreto: ");
-			}else if(senha != this.senha) {
-				System.out.print("Senha incorreta: ");
-			}else {
-				System.out.println("Logado");
+			
+			if(this.logado == true) {
+				System.out.println("Você já está logado");
 				break;
+			}
+			
+			if(usuario.equals(this.usuario) && senha.equals(this.senha)) {
+				System.out.println("Logado!!!");
+				setLogado(true);
+				break;
+			}else {
+				System.out.println("Credenciais Inválidas");
+				System.out.print("Usuário: ");
+				usuario = entrada.nextLine();
+				
+				System.out.print("Senha: ");
+				senha = entrada.nextLine();
 			}
 		}
 	}
 	
 	@Override
 	public void logout() {
-		//rete
+		if(this.logado == false) {
+			System.out.println("Você já está deslogado!");
+		}else {
+			System.out.println("Agora você deslogou");
+			setLogado(false);
+		}
 	}
-	
 	
 }
--- src/br/edu/fatecpg/tecprog/heranca/autenticavel/view/Main.java
+++ src/br/edu/fatecpg/tecprog/heranca/autenticavel/view/Main.java
@@ -1,5 +1,47 @@
 package br.edu.fatecpg.tecprog.heranca.autenticavel.view;
 
-public class Main {
+import br.edu.fatecpg.tecprog.heranca.autenticavel.model.SistemaDeSeguranca;
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		String usuario, senha;
+		int opcao;
+		boolean loop = true;
+		
+		SistemaDeSeguranca conta1 = new SistemaDeSeguranca();
+		
+		System.out.println("Opções\n1 - Logar\n2 - Deslogar\n0 - Fechar");
+		
+		while(loop) {
+			System.out.print("Opção: ");
+			opcao = entrada.nextInt();
+			entrada.nextLine();
+			switch(opcao) {
+				case 0:
+					loop = false;
+				break;
+				case 1:
+					
+					System.out.println("Informe o usuário: ");
+					usuario = entrada.nextLine();
+					
+					System.out.println("Informe a senha: ");
+					senha = entrada.nextLine();
+					
+					conta1.login(usuario, senha);
+				break;
+				case 2:
+					conta1.logout();
+				break;
+				default: 
+					System.out.println("Opção inválida");
+				break;
+					
+			}
+		}
+		
+		entrada.close();
+	}
 }
